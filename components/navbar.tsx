028814583965546fc6a8e928f64f3a00f5cba636
Vamshi <vamshivamsharaj123@gmail.com>
2026-02-16 01:14:15 +0530

    feat(auth): add user dropdown menu and sign out functionality

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,69 @@
+"use client"
 import { Briefcase, Ghost } from "lucide-react"
 import Link from "next/link";
 import { Button } from "./ui/button";
-export default function Navbar() {
+import { getSession } from "@/lib/auth/auth";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { signOut } from "better-auth/api";
+import { useSession } from "@/lib/auth/auth-client";
+import { useRouter } from "next/navigation";
+import SignOutButton from "./sign-out-btn";
+export default  function Navbar() {
+    const {data:session} =useSession();
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="container mx-auto px-4 flex items-center h-16 justify-between">
+
                 <Link href="/" className="flex gap-2 text-xl items-center font-semibold text-primary">
                     <Briefcase />
                     Job Tracker
                 </Link>
                 <div className="flex items-center gap-4">
-                    <Link href="/sign-in">
-                        <Button variant="ghost" className="text-gray-700 hover:text-black">
-                            Login
-                        </Button>
-                    </Link>
-                    <Link href="/sign-up">
-                        <Button  className="bg-primary hover:bg-primary/90">
-                            Start for free
-                        </Button>
-                    </Link>
+                    {session?.user ? (<>
+                        <Link href="/dashboard">
+                            <Button className="text-gray-700 hover:text-black" variant="ghost">
+                                DashBoard
+                            </Button>
+                        </Link>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger>
+                                <Button variant="ghost">
+                                    <Avatar>
+                                        <AvatarFallback className="bg-primary text-white">
+                                            {session.user.name[0].toUpperCase()}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent>
+                                <DropdownMenuLabel>
+                                    <div>
+                                        <p>{session.user.name}</p>
+                                        <p>{session.user.email}</p>
+                                    </div>
+                                </DropdownMenuLabel>
+                               <SignOutButton />
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </>) :
+                        (<>
+                            <Link href="/sign-in">
+                                <Button variant="ghost" className="text-gray-700 hover:text-black">
+                                    Login
+                                </Button>
+                            </Link>
+                            <Link href="/sign-up">
+                                <Button className="bg-primary hover:bg-primary/90">
+                                    Start for free
+                                </Button>
+                            </Link></>)}
                 </div>
             </div>
         </nav>);
--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
-
+import { headers } from "next/headers";
 const client=new MongoClient(process.env.MONGODB_URI!);
 const db=client.db();
 
@@ -13,3 +13,10 @@ export const auth=betterAuth({
         enabled:true,
     },
 })
+
+export async function getSession() {
+    const result=await auth.api.getSession({
+        headers: await headers(),
+    });
+    return result;
+}
